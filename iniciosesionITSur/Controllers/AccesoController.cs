2dae83110cdaa9127ace1e9b6eba96328283ca07
terrazfacundo <terrazfacundo06@gmail.com>
2024-10-17 12:26:35 -0300

    Conexion a la BD+Services, Acceso y Usuario.cs

---
+++ iniciosesionITSur/Controllers/AccesoController.cs
@@ -0,0 +1,19 @@
+﻿using ConexionDB;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iniciosesionITSur.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string UserId, string Contrasenia)
+        {
+            Usuarios usuarios = new Usuarios().encontrarUsuario(UserId, Contrasenia);
+        }
+    }
+}
